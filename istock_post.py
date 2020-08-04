b513395541579bcde914a80bd5bd85429bb59684
Finrod Chen <finrodchen@guangli.com.tw>
2020-08-04 18:44:41 +0800

    update

--- istock_post.py
+++ istock_post.py
@@ -19,12 +19,12 @@ post_content_html = post_content_html.encode("utf-8")
 wp = Client('https://i-stock.xyz/xmlrpc.php', 's02260441', 'simontp6nm06')
 
 post = WordPressPost()
-post.post_status = 'draft'
+post.post_status = 'publish'
 post.title = post_title
 post.content =post_content_html
 post.slug = post_title
 post.terms_names = {
-    'category' : ['投資小學堂']
+    'category' : ['COVID-19','美國新聞']
 }
 
 wp.call(NewPost(post))
